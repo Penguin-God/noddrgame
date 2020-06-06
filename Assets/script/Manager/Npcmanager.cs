6aa1f32e6a6deabb662e6f16a4cde7438bd9092c
sktt1-win <60744738+sktt1-win@users.noreply.github.com>
2020-06-06 20:57:05 +0900

    npc이동구현

--- Assets/script/Manager/Npcmanager.cs
+++ Assets/script/Manager/Npcmanager.cs
@@ -15,11 +15,16 @@ public class NPCMove
     public int frequency; //npc가 얼마나 지정된 방향으로 빈번하게 움직일 것인가
 }
 
-public class Npcmanager : MonoBehaviour
+public class Npcmanager : play
 {
     //커스텀 class가 인스펙터 창에 나오게 하기위한 명령어
     [SerializeField]
     public NPCMove npc;
+
+    private void Start()
+    {
+        StartCoroutine(MoveCoroutine());
+    }
 
     public void SetMove()
     {
@@ -32,7 +37,12 @@ public class Npcmanager : MonoBehaviour
     }
 
 
-    //IEnumerator은 코루틴 중에 하나로 yield return 구문을 어디엔가 포함하고 있는 함수이다.
+    //IEnumerator은 코루틴(Coroutine) 중에 하나로 yield return 구문을 어디엔가 포함하고 있는 함수이다.
+    //yield return 구문을 포함하지 않으면 에러가 난다.
+    //yield return : 실행을 중지하고 다음 프레임에서 실행을 재개할 수 있는 지점
+    //코루틴을 실행하려면 StartCoroutine 함수를 사용하면 됨
+    //코루틴 기본코드 IEnumerator 함수명(){code} 
+    //코루틴 사용코드 StartCoroutine(코루틴함수명());
     IEnumerator MoveCoroutine()
     {
         if(npc.direction.Length != 0)
@@ -54,8 +64,19 @@ public class Npcmanager : MonoBehaviour
                         yield return new WaitForSeconds(1f);
                         break;
                     case 5:
+                        //yield return new WaitForSeconds(); 값이 없을 경우 대기시간없이 무한히 함수가 반복되므로 렉걸려서 튕김
                         break;
                 }
+
+                //NpcCanMove가 true가 될 때까지 무한대기
+                //case5의 경우 대기시간이 없어 무한반복되어 렉걸리는데 코르틴이 끝날때마다 NpcCanMove를 true로 만들고 끝날때마다 실행시키기 때문에
+                //캐릭터는 딜레이없이 움직이면서 유니티도 안튕김
+                yield return new WaitUntil(() => NpcCanMove);
+                // base : 자식이 상속받는 부모가 있을 시 부모의 메서드를 사용할 때 사용하는 접근자 ex) base.부모매서드명()
+                base.Move(npc.direction[i]);
+
+                if (i == npc.direction.Length - 1)
+                    i = -1;
             }
         }
     }
--- Assets/script/play.cs
+++ Assets/script/play.cs
@@ -10,10 +10,51 @@ public class play : MonoBehaviour
     protected float h;
     protected float v;
     protected bool XMove;
+    protected bool NpcCanMove = true;
+
+    public int walkcount;
+    protected int count;
 
     //protected : 부모자식간의 상속은 가능하지만 인스펙터 창에서 노출은 되지않는 보호수준
     protected Rigidbody2D Rigid;
     protected Vector3 dirvec;
+
+    protected void Move(string _dir)
+    {
+        StartCoroutine(MoveCoroutine(_dir));
+    }
+
+    IEnumerator MoveCoroutine(string _dir)
+    {
+        NpcCanMove = false;
+        dirvec.Set(0, 0, dirvec.z);
+
+        //switch 문에는 string값이 와도 상관이 없음
+        switch (_dir)
+        {
+            case("UP"):
+                dirvec.y = 1f;
+                break;
+            case ("DOWN"):
+                dirvec.y = -1f;
+                break;
+            case ("RIGHT"):
+                dirvec.x = 1f;
+                break;
+            case ("LEFT"):
+                dirvec.x = -1f;
+                break;
+        }
+
+        while(count < walkcount)
+        {
+            transform.Translate(dirvec.x * speed, dirvec.y * speed, 0);
+            count++;
+            yield return new WaitForSeconds(0.01f);
+        }
+        count = 0;
+        NpcCanMove = true;
+    }
 }
 
 
