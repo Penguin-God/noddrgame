7a0d2c35df7354d144eca7d27b578aada5188d5b
sktt1-win <60744738+sktt1-win@users.noreply.github.com>
2020-08-05 23:12:21 +0900

    Fadein and Fadeout
    맵이동시 화면이 어두어진 후 다시 밝아지는 기능 추가.

---
+++ Assets/script/Manager/Fademanager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fademanager : MonoBehaviour
+{
+    public SpriteRenderer White;
+    public SpriteRenderer Black;
+    private Color color;
+
+    private WaitForSeconds FadeoutWaitTime = new WaitForSeconds(0.01f);
+    //while문 안에 new연산자를 쓰면 메모리 손해이기 때문에 위쪽에 생성(원래 코루틴 안에 new를 쓰기도 함)
+
+    public void FadeOut(float Speed = 0.02f) //아무값도 넣지 않으면 Speed는 0.02f임
+    {
+        StopAllCoroutines();//코루틴이 겹치면서 혼선이 일어날 수 있으니 함수 시작 시 전에 돌아가던 모든 코루틴을 멈춤
+        StartCoroutine(FadeOutCoroutine(Speed));
+    }
+
+    IEnumerator FadeOutCoroutine(float Speed)
+    {
+        color = Black.color;
+        while(color.a < 1f)// a = 알파(투명도)값
+        {
+            color.a += Speed;
+            Black.color = color; 
+            yield return FadeoutWaitTime; //알파값에 Speed를 계속 더하고 그값을 Black에 준 후 0.01f만큼의 대기시간을 가짐(약 1초동안 이 과정이 이루어짐)
+            //코루틴의 WaitForSeconds라는 기능의 유용성 때문에 자주 사용
+        }
+    }
+
+    //위쪽의 코드를 거의 그대로 갖고옴
+    public void FadeIn(float Speed = 0.02f) //아무값도 넣지 않으면 Speed는 0.02f임
+    {
+        StopAllCoroutines();
+        StartCoroutine(FadeInCoroutine(Speed));
+    }
+
+    IEnumerator FadeInCoroutine(float Speed)
+    {
+        color = Black.color;
+        while (color.a > 0f)//a = 0이 되면 검은색화면이 없어졌다는 의미이므로 while문 정지
+        {
+            color.a -= Speed;
+            Black.color = color;
+            yield return FadeoutWaitTime; 
+        }
+    }
+}
--- Assets/script/맵이동/mapchange.cs
+++ Assets/script/맵이동/mapchange.cs
@@ -7,19 +7,34 @@ public class mapchange : MonoBehaviour
 {
     public Transform MapChangePoint;
     private 변수저장소 Theplayer;
+    private Fademanager fademanager;
+    private Gamemanager gamemanager;
 
     private void Start()
     {
         //FindObjectOfType : 유니티 계층에 있는 모든 객체를 참조해서 가져옴 Getcomponent와 검색 범위가 다름
         Theplayer = FindObjectOfType<변수저장소>();
+        fademanager = FindObjectOfType<Fademanager>();
+        gamemanager = FindObjectOfType<Gamemanager>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name == "player")
         {
-            //Transform 변수에 게임 오브젝트를 넣어 충돌 시 그 오브젝트 위치로 이동하게 함 
-            Theplayer.transform.position = MapChangePoint.transform.position;
+            StartCoroutine(MapTransCoroutine());
         }
+    }
+
+    IEnumerator MapTransCoroutine()
+    {
+        gamemanager.isaction = true;
+        fademanager.FadeOut();
+        yield return new WaitForSeconds(1f);
+        //Transform 변수에 게임 오브젝트를 넣어 충돌 시 그 오브젝트 위치로 이동하게 함 
+        Theplayer.transform.position = MapChangePoint.transform.position;
+        fademanager.FadeIn();
+        yield return new WaitForSeconds(0.5f);
+        gamemanager.isaction = false;
     }
 }
