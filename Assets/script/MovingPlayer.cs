2106e09060e089b3d4d47784d9282d29b5736ae2
sktt1-win <parkjun141@gmail.com>
2020-08-20 22:22:47 +0900

    애니메이션 틀 제작

--- Assets/script/MovingPlayer.cs
+++ Assets/script/MovingPlayer.cs
@@ -17,35 +17,53 @@ public class MovingPlayer : MonoBehaviour
     private bool Canmove = true;
 
     Vector3 vector;
+    private Animator animator; 
 
-    IEnumerator playerCoroutine()
+    private void Start()
     {
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            ApplyRunspeed = Runspeed;
-            CanRun = true;
-        }
-        else
-        {
-            ApplyRunspeed = 0;
-            CanRun = false;
-        }
+        animator = GetComponent<Animator>(); //animator변수에 오브젝트에 있는 애니메이터를 담는 것 이것을 하지않으면 animator변수는 그냥 빈 껍데기
+    }
 
-        vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);//방향에 따라 각각 -1,1을리턴
-
-        while (Currentwalkcount < Walkcount)
+    IEnumerator PlayerCoroutine()
+    {
+        //Update에서만 입력받으면 프레임에 따라 애니메이션이 입력했다가 마지막에 끊기기 때문에 코루틴 내에서 while문을 넣어 자연스럽게 만듬
+        while (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0) 
         {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                ApplyRunspeed = Runspeed;
+                CanRun = true;
+            }
+            else
+            {
+                ApplyRunspeed = 0;
+                CanRun = false;
+            }
+
+            vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);//방향에 따라 각각 -1,1을리턴
+
             if (vector.x != 0)
-                transform.Translate(vector.x * (Speed + ApplyRunspeed), 0, 0);
-            else if (vector.y != 0)
-                transform.Translate(0, vector.y * (Speed + ApplyRunspeed), 0);
-            //Translate() : 현재 있는 값에서 ()안에 값만큼 더하는 함수
-            if (CanRun)
+                vector.y = 0;
+
+            animator.SetFloat("DirX", vector.x); //DirX에 vector.x의 값을 받겠다.
+            animator.SetFloat("DirY", vector.y);
+            animator.SetBool("Walking", true);
+
+            while (Currentwalkcount < Walkcount)
+            {
+                if (vector.x != 0)
+                    transform.Translate(vector.x * (Speed + ApplyRunspeed), 0, 0);
+                else if (vector.y != 0)
+                    transform.Translate(0, vector.y * (Speed + ApplyRunspeed), 0);
+                //Translate() : 현재 있는 값에서 ()안에 값만큼 더하는 함수
+                if (CanRun)
+                    Currentwalkcount++;
                 Currentwalkcount++;
-            Currentwalkcount++;
-            yield return new WaitForSeconds(0.01f);
+                yield return new WaitForSeconds(0.01f);
+            }
+            Currentwalkcount = 0;
         }
-        Currentwalkcount = 0;
+        animator.SetBool("Walking", false);
         Canmove = true;
     }
 
@@ -56,7 +74,7 @@ public class MovingPlayer : MonoBehaviour
             if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)//방향키를 입력받았을 때
             {
                 Canmove = false;
-                StartCoroutine(playerCoroutine());
+                StartCoroutine(PlayerCoroutine());
             }
         }
     }
