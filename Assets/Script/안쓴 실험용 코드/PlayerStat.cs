76a2bd249094f114f9f09b65cf543e91a1c4b893
Penguin-God <gkswh4860@naver.com>
2020-09-28 22:04:42 +0900

    Hp, Mp 구현

---
+++ Assets/Script/안쓴 실험용 코드/PlayerStat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStat : MonoBehaviour
+{
+    public static PlayerStat playerStat;
+
+    public int MaxHp;
+    public int CurrentHp;
+    public int MaxMp;
+    public int CurrentMp;
+
+    void Start()
+    {
+        playerStat = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
