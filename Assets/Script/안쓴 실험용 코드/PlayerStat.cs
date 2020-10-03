76d9a0358067b3db71bfd3610e41e094ecdaf929
sktt1-win <gkswh4860@naver.com>
2020-10-03 13:24:53 +0900

    게임 시작 UI 연출 구현

--- Assets/Script/안쓴 실험용 코드/PlayerStat.cs
+++ Assets/Script/안쓴 실험용 코드/PlayerStat.cs
@@ -6,15 +6,13 @@ using UnityEngine.UI;
 public class PlayerStat : MonoBehaviour
 {
     public int maxHp;
-    public int HP;
-    public int maxMp;
-    public int MP;
-
+    public int CurrentHp;
+   
     public Slider hpSilder;
 
     void Update()
     {
         hpSilder.maxValue = maxHp; 
-        hpSilder.value = HP; 
+        hpSilder.value = CurrentHp; 
     }
 }
---
+++ Assets/Script/안쓴 실험용 코드/실험용버튼.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class 실험용버튼 : MonoBehaviour
+{
+    public GameObject 시작창;
+    public GameObject 타이틀;
+
+    public PlayerStat stat;
+    public Playermanager playermanager;
+    public Gamemanager gamemanager;
+
+    public void GameExit()
+    {
+        Application.Quit();
+    }
+
+    public void 시작창뛰우기()
+    {
+        시작창.SetActive(true);
+    }
+
+    public void GameStart()
+    {
+        타이틀.SetActive(false);
+        시작창.SetActive(false);
+
+        StartCoroutine(HpAdd());
+    }
+
+    public void StartCancel()
+    {
+        시작창.SetActive(false);
+    }
+
+    IEnumerator HpAdd()
+    {
+        playermanager.isaction = true;
+        while(stat.CurrentHp < stat.maxHp)
+        {
+            stat.CurrentHp += 5;
+            yield return new WaitForSeconds(0.5f);
+        }
+        playermanager.isaction = false;
+    }
+}
