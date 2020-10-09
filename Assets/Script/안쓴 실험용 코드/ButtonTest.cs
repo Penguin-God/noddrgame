e1b38165052bb5b16c64134173c27eebae0c80e0
Penguin-God <gkswh4860@naver.com>
2020-10-09 23:42:38 +0900

    주석 정리

--- Assets/Script/안쓴 실험용 코드/ButtonTest.cs
+++ Assets/Script/안쓴 실험용 코드/ButtonTest.cs
@@ -48,29 +48,6 @@ public class ButtonTest : MonoBehaviour
         시작창.SetActive(false);
     }
 
-    //IEnumerator GameStartTalk()
-    //{
-    //    gamemanager.컷씬대화(700);
-    //    while (stat.CurrentHp < stat.maxHp)
-    //    {
-    //        yield return new WaitUntil(() => Input.GetButtonDown("Jump"));
-    //        stat.CurrentHp += 17;
-    //        Camera.transform.position += new Vector3(0, 3, 0);
-    //        yield return new WaitForSeconds(0.05f);
-    //    }
-    //    yield return new WaitForSeconds(1.5f);
-    //    gamemanager.컷씬대화(800);
-    //}
-
-    //IEnumerator UIOut(float Speed)
-    //{
-    //    while (color.a > 0f)
-    //    {
-    //        color.a -= Speed;
-    //        BlackImg.color = color;
-    //        yield return UIOutTime; 
-    //    }
-    //}
 
     IEnumerator GameStartCut(float Speed)
     {
