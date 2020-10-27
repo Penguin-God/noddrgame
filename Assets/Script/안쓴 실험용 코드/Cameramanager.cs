dfd749ee4c9184d2fa87801c03ac618c0d2e6ee2
Penguin-God <gkswh4860@naver.com>
2020-10-27 11:57:52 +0900

    맵 변경 시 카메라 제한구역 변경하는 함수 제작

--- Assets/Script/안쓴 실험용 코드/Cameramanager.cs
+++ Assets/Script/안쓴 실험용 코드/Cameramanager.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 
 public class Cameramanager : MonoBehaviour
 {
-    public PolygonCollider2D polygon;
-    CinemachineConfiner Confiner;
+    public CinemachineConfiner Confiner;
 
     private void Awake()
     {
         Confiner = GetComponent<CinemachineConfiner>();
     }
 
-    public void CollderChange(PolygonCollider2D newBox)
+    public void CollderChange(PolygonCollider2D newBox) 
     {
-        Confiner.m_BoundingShape2D = newBox; // 제한 콜라이더를 MpaChange스크립트가 가지고 있는 콜라이더로 변경
+        if (Confiner.m_BoundingShape2D != newBox)
+            Confiner.m_BoundingShape2D = newBox;
     }
 }
