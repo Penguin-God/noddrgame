fd1dfff7ae0cb220e1e919bef5b880ef1fb72e75
Penguin-God <gkswh4860@naver.com>
2020-11-14 21:22:10 +0900

    create script

---
+++ Assets/Script/안쓴 실험용 코드/QuestionData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class QuestionData
+{
+    public string questionName;
+    public int questionId;
+
+    public QuestionData(string name, int id)
+    {
+        questionName = name;
+        questionId = id;
+    } 
+}
---
+++ Assets/Script/안쓴 실험용 코드/Questionmanager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Questionmanager : MonoBehaviour
+{
+    public int questionId;
+
+    Dictionary<int, QuestionData> questionList;
+
+    private void Awake()
+    {
+        questionList = new Dictionary<int, QuestionData>();
+        AddData();
+    }
+
+    void AddData()
+    {
+        questionList.Add(10, new QuestionData("잠 여부", 200));
+
+    }
+}
