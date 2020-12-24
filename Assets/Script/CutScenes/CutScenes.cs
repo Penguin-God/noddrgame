cc4fdda5dd4eb2218c01cf8ae00455bbe4499208
Penguin-God <gkswh4860@naver.com>
2020-12-24 16:05:54 +0900

    improve UI control

--- Assets/Script/CutScenes/CutScenes.cs
+++ Assets/Script/CutScenes/CutScenes.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class CutScenes : MonoBehaviour
 {
     public Cameramanager cameramanager;
-    public PlayerStat stat;
+    public PlayerStat playerStat;
     public Gamemanager gamemanager;
     public Fademanager fademanager;
+    public Playermanager playermanager;
 
     private Animator animator;
 
     private void Awake()
     {
-        animator = stat.GetComponent<Animator>();
+        animator = playerStat.GetComponent<Animator>();
     }
 
     public void StartCut(float speed)
@@ -27,17 +28,19 @@ public class CutScenes : MonoBehaviour
         yield return new WaitUntil(() => fademanager.color.a < 0.5f);
         // 대사 시작
         gamemanager.CutSceneTalk(700);
-        for (int i = 0; i < 2; i++)
-        {
-            int index = gamemanager.talkindex;
-            cameramanager.CameraMove(new Vector3(0, 4.5f, 0), 0.1f, 15);
-            stat.CurrentHp += 24;
-            animator.SetBool("testDDR", true);
-            yield return new WaitUntil(() => !cameramanager.isCameraMove && index != gamemanager.talkindex);
-        }
+        animator.SetBool("testDDR", true);
+        yield return new WaitUntil(() => !playermanager.isaction);
+        StartCoroutine(playerStat.AddHp(4, 0.15f));
+        yield return new WaitUntil(() => playerStat.PlayerDie);
         animator.SetBool("testDDR", false);
-        stat.CurrentHp += 2;
-        yield return new WaitForSeconds(1.4f);
         gamemanager.CutSceneTalk(800);
     }
+
+    // 대사 넘어갈 때마다 카메라 이동시키는 코드
+    //for (int i = 0; i < 2; i++)
+    //{
+    //    int index = gamemanager.talkindex;
+    //    cameramanager.CameraMove(new Vector3(0, 4.5f, 0), 0.1f, 15);
+    //    yield return new WaitUntil(() => index != gamemanager.talkindex);
+    //}
 }
--- Assets/Script/UI/PlayerStat.cs
+++ Assets/Script/UI/PlayerStat.cs
@@ -11,7 +11,7 @@ public class PlayerStat : MonoBehaviour
     public int CurrentHp;
 
     public bool PlayerDie;
-    bool addHp; // HP가 오르고 있을 때 true
+    bool hpAdding; // HP가 오르고 있을 때 true
 
     public Slider hpBar;
     public UIRotate UIrotate;
@@ -21,28 +21,43 @@ public class PlayerStat : MonoBehaviour
         hpBar.maxValue = maxHp; 
         hpBar.value = CurrentHp;
         PlayerDeat();
-        if (Input.GetButtonDown("Jump") && addHp) // hp 만땅 안되게 space눌러서 hp낮춤
-            CurrentHp += 5;
+        if (Input.GetButtonDown("Jump") && hpAdding) // hp 만땅 안되게 space눌러서 hp낮춤
+            CurrentHp -= 5;
     }
 
     void PlayerDeat()
     {
-        if (CurrentHp >= maxHp)
-            StartCoroutine(HpSubtract());
+        if (CurrentHp >= maxHp && !hpAdding)
+            HpSubtract();
     }
 
-    IEnumerator HpSubtract()
+    void HpSubtract()
     {
-        addHp = true;
         UIrotate.ValueRotation(new Vector3(0, 0, 180)); // HpBar 180도 회전
-        PlayerDie = true;
         playermanager.isaction = true;
+        StartCoroutine(SubHp(5, 0.07f));
+    }
+
+    public IEnumerator AddHp(int AddHp, float WaitTime) 
+    {
+        hpAdding = true;
+        while (CurrentHp <= 50)
+        {
+            CurrentHp += AddHp;
+            yield return new WaitForSeconds(WaitTime);
+        }
+        CurrentHp = 51;
+        hpAdding = false;
+    }
+
+    IEnumerator SubHp(int SubHp, float WaitTime) 
+    {
         while (CurrentHp > 0)
         {
-            CurrentHp -= 5;
-            yield return new WaitForSeconds(0.5f);
+            CurrentHp -= SubHp;
+            yield return new WaitForSeconds(WaitTime);
         }
         CurrentHp = 0;
-        addHp = false;
+        PlayerDie = true;
     }
 }
