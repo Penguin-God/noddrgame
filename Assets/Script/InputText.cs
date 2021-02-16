e8564128c26f36e999fff0e1575af8dd56ff169f
Penguin-God <gkswh4860@naver.com>
2021-02-16 14:46:03 +0900

    test InputField

---
+++ Assets/Script/InputText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputText : MonoBehaviour
+{
+    public Text nickname;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log(nickname.text);
+            Destroy(this.gameObject);
+        }
+    }
+}
